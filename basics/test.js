22bf1e36b25ac218a4536bf7faa8edb5f4b126ab
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-20 16:23:48 +0000

    comparisons

---
+++ basics/test.js
@@ -0,0 +1,21 @@
+// ****************operations**************
+
+let v = 3
+let nv = -v
+console.log(nv)
+
+console.log("1" + 2 + 2)
+console.log(+true)
+
+console.log(2 < 1)
+
+console.log("2" > 1)
+
+console.log(null > 0)
+console.log(null == 0)
+console.log(null >= 0)
+
+
+
+
+
