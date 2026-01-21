de62d594ad426d3baf6aa4916b77f591fb72dc79
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-21 15:29:02 +0000

    datatypes

--- basics/test.js
+++ basics/test.js
@@ -1,21 +1,42 @@
 // ****************operations**************
 
-let v = 3
-let nv = -v
-console.log(nv)
+// let v = 3
+// let nv = -v
+// console.log(nv)
 
-console.log("1" + 2 + 2)
-console.log(+true)
+// console.log("1" + 2 + 2)
+// console.log(+true)
 
-console.log(2 < 1)
+// console.log(2 < 1)
 
-console.log("2" > 1)
+// console.log("2" > 1)
 
-console.log(null > 0)
-console.log(null == 0)
-console.log(null >= 0)
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+
+//-------------------datatypes-----------------
+
+const id = Symbol("123")
+const id2 = Symbol("123")
+
+console.log(id === id2)
+
+const heroes = ["shakti", "mukti", "nagraj"] //Arrays
+
+//Objects
+let myObj = {
+    name: "aftab",
+    roll: 39,
+}
+console.log(myObj)
 
 
+const myFunction = Function()
+{
+    console.log("hello")
+}
+myFunction()
 
 
 
