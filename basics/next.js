623da9899e18cb440b4aa07a71245eade9714f54
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-21 16:29:23 +0000

    string

---
+++ basics/next.js
@@ -0,0 +1,21 @@
+let userOne = {
+    name: "aftab",
+    roll: 39,
+}
+let userTwo = userOne
+userTwo.name = "shlok"
+console.log(userOne.name)
+console.log(userTwo.name)
+
+const nameOne = new String('aftab')
+console.log(nameOne[2])
+console.log(nameOne.__proto__)
+console.log(nameOne.length)
+console.log(nameOne.toUpperCase())
+console.log(nameOne.charAt(3))
+console.log(nameOne.indexOf('a'))
+
+
+
+
+
