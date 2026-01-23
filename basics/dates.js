44d5a4cf0cfc759ff91e18c82466c57a88011ec3
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-23 15:41:14 +0000

    dates

---
+++ basics/dates.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+console.log(myDate.toDateString())
+console.log(myDate.toLocaleString())
+
+console.log(typeof myDate)
+
+let myCreateDate = new Date(2026, 0, 23)
+//months start from 0 in javascript
+// (yyyy mm dd hh mm ss) Syntax
+console.log(myCreateDate.toDateString())
+
+
+
