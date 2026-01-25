7979c868277e070073a2bde472a5dc5bf72d16c7
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-25 16:10:35 +0000

    objects

---
+++ basics/objects.js
@@ -0,0 +1,24 @@
+const course = {
+    c_name : "JS",
+    price : "100",
+    c_instr : "aftab"
+}
+const {c_instr} = course
+
+console.log(c_instr)
+
+{
+    "name": "aftab",
+    "coursename": "JS",
+    "cost": "free"
+}
+
+
+
+
+
+
+
+
+
+
