c8aee293f9f1f77daf50de257f3f81250b11b6c7
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-29 16:12:25 +0000

    scope

---
+++ basics/scope.js
@@ -0,0 +1,26 @@
+function one(){
+    const username = "hitesh"
+    function two(){
+        const web = "google"
+        console.log(username);
+        
+    }
+    //console.log(web);
+    two()
+}
+one()
+
+//-------------------------------------------
+
+function addone(num){
+    return num + 1
+}
+
+console.log(addone(5))
+
+
+
+
+
+
+
