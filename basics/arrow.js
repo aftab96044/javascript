3762e0085095f45077efb8af0f2ef5421a962b19
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-01-31 15:34:04 +0000

    arrow

--- basics/arrow.js
+++ basics/arrow.js
@@ -1,3 +1,29 @@
 const user = {
-    
+    username : "aftab",
+    price : 999,
+    welcomeMessage: function(){
+        console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
+    }
 }
+user.welcomeMessage()
+user.username = "sam"
+user.welcomeMessage()//will replace the value of username with sam
+
+console.log(this);
+
+// function chai(){
+//     console.log(this);
+    
+// }
+// chai()
+
+
+
+
+
+
+
+
+
