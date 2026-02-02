886494c71250ed2dbddade9ce98c983337acb3bb
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-02-02 16:07:24 +0000

    nullish

--- controlflow/one.js
+++ controlflow/one.js
@@ -8,8 +8,24 @@ const bal = 5000
 if(bal > 100) console.log("eufih");
 
 
+//falsy values
+// false, 0, -0, BigInt, 0n, null, undefined, NaN
 
+//truthy values
+// "0", 'false', " ", {}, [], function(){}
 
+const emptyObj = {}
+if(Object.keys(emptyObj).length === 0){
+    console.log("object is empty");
+    
+}
+// -----------------------------------------
+// Nullish coalescing Operator (??) : null undefined
+let val1;
+val1 = 5 ?? 10 //for DB 
+val2 = null ?? 7 //checks for safe value
+
+console.log(val1, " ", val2);
 
 
 
