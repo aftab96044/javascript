9fe1f158bc8c40b15ea81cd6a05f9db3b0fec99d
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-02-03 16:00:11 +0000

    loops

---
+++ controlflow/loops.js
@@ -0,0 +1,19 @@
+//-----------for----------
+
+for (let index = 0; index < 10; index++) {
+    const element = index;
+    console.log(element);
+    
+}
+
+
+for (let i = 0; i <= 10; i++) {
+    console.log(`Outer loop ${i}`);
+    
+    for (let j = 0; j <= 10; j++) {
+        console.log(`Inner loop ${j} and inner loop ${i}`);
+        
+        
+    }
+    
+}
