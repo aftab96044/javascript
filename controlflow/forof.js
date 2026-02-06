7e6c0630ddf79afbc22a1a50b0caf886b513aa5d
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-02-06 16:15:05 +0000

    forof

---
+++ controlflow/forof.js
@@ -0,0 +1,66 @@
+//for of
+
+const arr = [1, 2, 3, 4, 5]
+
+for (const i of arr) {
+    console.log(i);
+    
+}
+
+const greet = "Hello world"
+
+for (const g of greet) {
+    console.log(`Each char is ${g}`);
+    
+}
+console.log();
+
+// Maps not iterable
+//Unique values
+const map = new Map()
+
+map.set('IN', "India")
+map.set('USA', "United states of america")
+map.set('UN', "United Nations")
+map.set('IN', "India" ) //won't print
+
+console.log(map);
+
+for (const [key, value] of map) {
+    console.log(key, " :- ", value );
+    
+}
+console.log();
+
+const myObj = {
+    'game1' : 'NFS',
+    'game2' : 'Spiderman'
+}
+
+for (const [key , value] of myObj) {
+    console.log(key,':-',value);
+    
+}
+// object is not iterable in forof so we will use 'forof'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
