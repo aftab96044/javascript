b6876482732ab602e4f14244bc962f05a50e182a
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-02-06 16:15:45 +0000

    foreach

---
+++ controlflow/foreach.js
@@ -0,0 +1,54 @@
+const coding = ["js", "ruby", "java", "py", "cpp"]
+
+coding.forEach( function (item){
+    console.log(item);
+    
+} )
+console.log();
+
+coding.forEach( (item) => {
+    console.log(item);
+    
+} )
+console.log();
+
+function printMe(item){
+    console.log(item);
+    
+}
+coding.forEach(printMe)
+
+console.log();
+
+coding.forEach( (item, index, arr) => {
+    console.log(item, index, arr);
+    
+} )
+
+const myCoding = [
+    {
+        languageName: "javascript",
+        fileNme: "js"
+    },
+    {
+        languageName: "java",
+        fileNme: "java"
+    },
+    {
+        languageName: "python",
+        fileNme: "py"
+    }
+]
+
+myCoding.forEach( (item) => {
+    console.log(item.fileNme);
+    
+})
+
+
+
+
+
+
+
+
