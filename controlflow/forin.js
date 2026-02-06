30274915c500c2dd5fa56a0f8e6e87833669965f
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-02-06 16:15:27 +0000

    forin

---
+++ controlflow/forin.js
@@ -0,0 +1,34 @@
+const Object = {
+    js: 'javascript',
+    cpp: 'C++',
+    rb: 'ruby',
+    sw: 'swift'
+
+}
+for (const key in Object) {
+    console.log(`${key} shortcut for ${Object[key]}`);
+        
+}
+
+const prog = ["js", "rb", "py", "java", "cpp" ]
+
+for (const key in prog) {
+    console.log(prog[key]);
+    //In arrays key is always number starting 0
+    //Whichh is why Objects was introduced , in objects we put anything as key 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
