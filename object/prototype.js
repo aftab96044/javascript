8fa1f082790c5008e66ab7f7709eefd2f86c1261
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-04-04 05:39:52 +0000

    prototype

---
+++ object/prototype.js
@@ -0,0 +1,32 @@
+// let myName = "  aftab  "
+
+// console.log(myName.length);
+// console.log(myName);
+
+
+let myHeroes = ["joker", "batman"]
+
+let heroPower = {
+    joker: "knife",
+    batman: "bike",
+
+    getjokerPower: function(){
+        console.log(`joker has ${this.joker}`);
+        
+    }
+}
+
+Object.prototype.aftab = function(){
+    console.log(`aftab is present `);
+    
+}
+
+heroPower.aftab()
+
+
+
+
+
+
+
+
