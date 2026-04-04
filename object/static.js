9227874b0b72f7f3986c7d49edecbe5c2b660d55
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-04-04 10:13:49 +0000

    static

---
+++ object/static.js
@@ -0,0 +1,35 @@
+class User{
+    constructor(username){
+        this.username = username
+    }
+
+    logMe(){
+        console.log(`Username: ${this.username}`);
+        
+    }
+
+    static createId(){
+        return `123`
+    }
+}
+
+
+const aftab = new User("aftab")
+console.log(User.createId())
+
+class Teacher extends User{
+    constructor(username, email){
+        super(username)
+        this.email = email
+    }
+}
+
+const iphone = new Teacher("iphone", "i@phone.com")
+console.log(iphone.createId())
+
+
+
+
+
+
+
