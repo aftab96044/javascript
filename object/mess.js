7bd820dd1047d2dfb15a7c00e397a7c29b8562c4
aftab96044 <143419080+aftab96044@users.noreply.github.com>
2026-06-05 09:49:49 +0000

    mess

---
+++ object/mess.js
@@ -0,0 +1,9 @@
+import {name, age} from './person.js';
+
+console.log(`my name is ${name} and i am ${age} years old.`)
+
+//JSX stands for JavaScript XML.
+
+//JSX allows us to write HTML in React.
+
+//JSX makes it easier to write and add HTML in React.
